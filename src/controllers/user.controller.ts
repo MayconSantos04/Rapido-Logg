792bf34f63888d795eeb559b687c26b062596aed
MayconSantos04 <gadsence47@gmail.com>
2026-07-31 16:33:05 -0300

    feat: addedencryption for password.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,19 @@
+import { z } from "zod";
+import { hash } from "bcrypt";
 import { NextFunction, Request, Response } from "express";
 
 class UserController {
-  create(request: Request, response: Response, next: NextFunction) {
-    return response.json({ message: "ok!" });
+  async create(request: Request, response: Response, next: NextFunction) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password } = bodySchema.parse(request.body);
+    const hashedPassword = await hash(password, 8)
+
+    return response.json({ message: "ok!", hashedPassword });
   }
 }
 
