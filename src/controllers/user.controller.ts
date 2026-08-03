83d14a93a0a6a721fa82428b859376df08cb2706
MayconSantos04 <gadsence47@gmail.com>
2026-08-03 12:41:40 -0300

    feat: created new routes and acontroller of sessions.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -31,7 +31,7 @@ class UserController {
 
     const { password: _, ...userWithoutPassword } = user;
 
-    return response.json(userWithoutPassword);
+    return response.status(201).json(userWithoutPassword);
   }
 }
 
