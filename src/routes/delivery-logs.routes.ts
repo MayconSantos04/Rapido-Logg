4759dca42f59cfa254aeccc604daed8102fc4608
MayconSantos04 <gadsence47@gmail.com>
2026-08-06 14:47:34 -0300

    feat: displayng details of order.

---
+++ src/routes/delivery-logs.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { DeliveryLogsController } from "@/controllers/delivery-logs.controller";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
+
+const deliveryLogsRoutes = Router();
+const deliveryLogsController = new DeliveryLogsController();
+
+deliveryLogsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  verifyUserAuthorization(["sale"]),
+  deliveryLogsController.create,
+);
+
+deliveryLogsRoutes.get(
+  "/:delivery_id/show",
+  ensureAuthenticated,
+  verifyUserAuthorization(["sale", "customer"]),
+  deliveryLogsController.show,
+);
+
+export { deliveryLogsRoutes };
