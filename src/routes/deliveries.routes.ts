9b4ac416f38a730f91e515ba9018323960416c4e
MayconSantos04 <gadsence47@gmail.com>
2026-08-06 11:50:19 -0300

    feat: updating the status of delivery.

--- src/routes/deliveries.routes.ts
+++ src/routes/deliveries.routes.ts
@@ -2,12 +2,16 @@ import { Router } from "express";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { DeliveriesController } from "@/controllers/deliveries.controller";
 import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
+import { DesliveryStatusController } from "@/controllers/deliveries-status.controller";
 
 const deliveriesRoutes = Router();
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DesliveryStatusController();
 
 deliveriesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["sale"]));
 deliveriesRoutes.post("/", deliveriesController.create);
 deliveriesRoutes.get("/", deliveriesController.index);
+
+deliveriesRoutes.patch("/:id/status", deliveriesStatusController.update);
 
 export { deliveriesRoutes };
